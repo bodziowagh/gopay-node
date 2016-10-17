7d90e11ae628d35ae21ff25b7b871acd2f96008e
kvnflm <kvnflm@gmail.com>
2016-10-17 16:01:17 +0900

    Start merchant-console branch

---
+++ lib/constants.d.ts
@@ -0,0 +1,6 @@
+export declare const DEFAULT_ENDPOINT: string;
+export declare const ENV_KEY_ENDPOINT: string;
+export declare const ENV_KEY_APP_ID: string;
+export declare const ENV_KEY_SECRET: string;
+export declare const POLLING_TIMEOUT: number;
+export declare const POLLING_INTERVAL: number;
